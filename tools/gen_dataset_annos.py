345d553217c2055c0631c8d1ba59bf1242fca646
lu8000 <118542382+lu8000@users.noreply.github.com>
2023-05-15 08:41:22 +0800

    Update gen_dataset_annos.py

--- tools/gen_dataset_annos.py
+++ tools/gen_dataset_annos.py
@@ -14,7 +14,7 @@ def gen_coco():
                 |- train2017
                 |- val2017
     '''
-    root_path = './datasets/coco'
+    root_path = '/kaggle/input/coco-2017-dataset/coco2017'
     save_path = './datasets/coco/'
     for mode in ('train', 'val'):
         im_root = osp.join(root_path, f'images/{mode}2017')
